a4ecb770e5a8b612f4f7a768c8912d151b40f814
Geovanni <geovannivelazco2000@gmail.com>
2025-10-22 15:04:57 -0600

    22 de oct

---
+++ Calculadora/Calculadora/src/Ui/MainMenu.java
@@ -0,0 +1,19 @@
+package Ui;
+import java.util.Scanner;
+public class MainMenu {
+    public static void Menu(){
+    Scanner sc = new Scanner(System.in);
+System.out.println("༻Bienvenida༺");
+System.out.println("1. Registro");
+System.out.println("2. Iniciar sesion");
+System.out.println("----------");
+int opt = sc.nextInt();
+switch (opt){
+case 0:
+System.out.println("gracias por su visita");
+break;
+case 1;
+break;
+case 2;
+break
+}
